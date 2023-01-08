455d31d12eff1d1a70a98319539ec617e5d47892
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2023-01-08 16:31:55 +0100

    Runtime without prints

--- lib/runtime.c
+++ lib/runtime.c
@@ -11,7 +11,7 @@ void printInt(long long i) {
 }
 
 void printString(char* str) {
-    fprintf(stderr, "printString arg: %p\n", str);
+    // fprintf(stderr, "printString arg: %p\n", str);
     unsigned long long const len = *(unsigned long long*)str;
     str += sizeof(unsigned long long);
     printf("%.*s\n", (int)len, str);
@@ -22,7 +22,7 @@ long long readInt() {
     scanf("%lli", &in);
     int i = getchar();
     if (i != '\n') {
-        fprintf(stderr, "int not followed by newline, nonsupported case. Terminating.\n");
+        fprintf(stderr, "RUNTIME: int not followed by newline, nonsupported case. Terminating.\n");
         exit(1);
     }
     return in;
@@ -34,10 +34,10 @@ char* readString() {
     size_t buf_len = 0;
     long long len = getline(&dst, &buf_len, stdin);
     if (len < 0) {
-        fprintf(stderr, "getline returned %lli; terminating.\n", len);
+        fprintf(stderr, "RUNTIME: getline returned %lli; terminating.\n", len);
         exit(1);
     }
-    fprintf(stderr, "Read len bytes: %lli\n", len);
+    // fprintf(stderr, "Read len bytes: %lli\n", len);
     dst = realloc(dst, len + sizeof(long long));
     memmove(dst + sizeof(long long), dst, len);
     *(unsigned long long*)dst = len - 1; // drop newline
@@ -45,21 +45,21 @@ char* readString() {
 }
 
 static char* __alloc_string(unsigned long long len) {
-    fprintf(stderr, "Allocating string for len %lli\n", len);
+    // fprintf(stderr, "Allocating string for len %lli\n", len);
     char *const str = malloc(len + sizeof(unsigned long long));
     if (!str) {
-        fprintf(stderr, "malloc returned NULL; terminating.\n");
+        fprintf(stderr, "RUNTIME: malloc returned NULL; terminating.\n");
         exit(1);
     }
     return str;
 }
 
 char* __concat_strings(char const*const restrict str1, char const*const restrict str2) {
-    fprintf(stderr, "Concatenating strings:\n");
+    // fprintf(stderr, "Concatenating strings:\n");
     unsigned long long const len1 = *(unsigned long long*)str1;
-    fprintf(stderr, "str1: len %lli, contents: \"%.*s\"\n", len1, (int)len1, str1 + sizeof(unsigned long long));
+    // fprintf(stderr, "str1: len %lli, contents: \"%.*s\"\n", len1, (int)len1, str1 + sizeof(unsigned long long));
     unsigned long long const len2 = *(unsigned long long*)str2;
-    fprintf(stderr, "str2: len %lli, contents: \"%.*s\"\n", len2, (int)len2, str2 + sizeof(unsigned long long));
+    // fprintf(stderr, "str2: len %lli, contents: \"%.*s\"\n", len2, (int)len2, str2 + sizeof(unsigned long long));
     unsigned long long const len3 = len1 + len2;
     char *const str3 = __alloc_string(len3);
     *(unsigned long long*)str3 = len3;
@@ -67,6 +67,6 @@ char* __concat_strings(char const*const restrict str1, char const*const restrict
     memcpy(str3_writer, str1 + sizeof(unsigned long long), len1);
     str3_writer += len1;
     memcpy(str3_writer, str2 + sizeof(unsigned long long), len2);
-    fprintf(stderr, "str3: len %lli, contents: \"%.*s\"\n", len3, (int)len3, str3 + sizeof(unsigned long long));
+    // fprintf(stderr, "str3: len %lli, contents: \"%.*s\"\n", len3, (int)len3, str3 + sizeof(unsigned long long));
     return str3;
 }
